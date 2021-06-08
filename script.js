a455ecf331b68ddf2b88cb37daf75e606c503d5d
Damon Rochon <>
2021-06-08 14:07:39 +0000

    whyyyy

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ let userguess = 0
 
 document.getElementById('button').addEventListener('click', randomizer)
 
- function randomizer () {
-
+function randomizer () {
   userguess = document.getElementById('creeper').value
   userguess = parseInt(userguess)
   randomnumber = (Math.random() * 6) + 1
   randomnumber = parseInt(randomnumber)
-
+  
   if (userguess === randomnumber) {
     document.getElementById('steve').innerHTML = "Congrats, You Are Big Brain!!!"
     }
