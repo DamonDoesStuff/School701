ff4b5e0204043f256890477bbad3bc30d8849b2d
Damon Rochon <>
2021-06-08 13:46:36 +0000

    i don't know

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ let userguess = 0
 document.getElementById('button').addEventListener('click',randomizer)
 
 function randomizer () {
+
   userguess = document.getElementById('creeper').value
   userguess = parseInt(userguess)
   randomnumber = (Math.random() * 6) + 1
   randomnumber = parseInt(randomnumber)
 
-  if (userguess == randomnumber) {
+  if (userguess === randomnumber) {
     document.getElementById('steve').innerHTML = "Congrats, You Are Big Brain!!!"
+    
   }
 }
